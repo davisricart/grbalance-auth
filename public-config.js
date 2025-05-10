da65ec9a940f746c56a57969274bc9a53eee6830
davisricart <davisricart@gmail.com>
2025-05-09 22:38:49 -0400

    Add files via upload

---
+++ public-config.js
@@ -0,0 +1,8 @@
+// public-config.js - This file is safe to commit to GitHub
+// Contains NO sensitive API keys or credentials
+
+// Project IDs are not considered sensitive
+const PROJECT_ID = "gr-balance";
+
+// This tells your app how to find the full configuration
+const CONFIG_SOURCE = "firebase-remote-config";  // Options: firebase-remote-config, local-file
